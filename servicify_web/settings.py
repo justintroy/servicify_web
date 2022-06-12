30c2c4a494255796c66eda03b08b752639300237
justintroy <xermasterz@gmail.com>
2022-06-12 08:35:43 +0800

    email from

--- servicify_web/settings.py
+++ servicify_web/settings.py
@@ -287,5 +287,8 @@ ANYMAIL = {
     "SENDINBLUE_API_KEY": os.getenv("SENDINBLUE_API_KEY", False),
 }
 
+DEFAULT_FROM_EMAIL = "xermasterz@gmail.com"
+SERVER_EMAIL = "xermasterz@gmail.com"  # ditto (default from-email for Django errors)
+
 # Set this true only when needed, it charges the API
 SMS_NOTIFICATION = os.getenv("USE_TWILIO", False)
